f3cd76968707c9c224b5be017fb38300957a7b43
Borhenmejri <borheneddine.mejri@esprit.tn>
2025-04-25 15:25:25 +0100

    first commit

---
+++ database/migrations/2025_04_25_141549_create_animals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('breed')->nullable();
+            $table->integer('age');
+            $table->string('location');
+            $table->text('health_notes')->nullable();
+            $table->text('feeding_schedule')->nullable();
+            $table->string('status')->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animals');
+    }
+};
